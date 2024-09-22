305141688426d7e65727a530c196e721e9a3b099
Samundar Singh <71628177+Samundar9525@users.noreply.github.com>
2024-09-22 20:09:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
